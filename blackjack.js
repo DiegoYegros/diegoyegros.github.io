dbad1b153074a7d6327e986f1a5680eac4148db0
Diego Yegros <diegoyegrosa@gmail.com>
2022-10-09 20:03:36 -0300

    delete background style counter

---
+++ blackjack.js
@@ -0,0 +1,33 @@
+/*let firstCard = 6
+let secondCard = 4
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card❓")
+}
+else if (sum === 21) {
+    console.log("You have got Blackjack! 🍾🥂")
+}
+else {
+    console.log("You are out of the game 😭")
+}
+
+/*
+let age = 21
+if (age < 21) {
+    console.log("You can not enter the club!")
+}
+else {
+    console.log("Welcome!")
+}
+
+let age = 100
+if (age < 100) {
+    console.log("Not elegible")
+}
+else if (age === 100) {
+    console.log("Here is your birthday card from the King!")
+}
+else {
+    console.log("Not elegible, you have already gotten one")
+}*/
--- index.js
+++ index.js
@@ -54,6 +54,19 @@ function multiply() {
 
 }
 
+let firstCard = 6
+let secondCard = 4
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card❓")
+}
+else if (sum === 21) {
+    console.log("You have got Blackjack! 🍾🥂")
+}
+else if (sum > 21) {
+    console.log("You are out of the game 😭")
+}
 
 /*let firstName = "Diego"
 let lastName = "Yegros"
