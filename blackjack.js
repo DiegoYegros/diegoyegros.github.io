dc695de6474d409e6aaf801fe95f33e248746908
Diego Yegros <diegoyegrosa@gmail.com>
2022-10-09 20:15:37 -0300

    more changes

--- blackjack.js
+++ blackjack.js
@@ -1,16 +1,34 @@
 let firstCard = 6
 let secondCard = 4
 let sum = firstCard + secondCard
-
+let hasBlackJack = false
+let isAlive = true
 if (sum < 21) {
     console.log("Do you want to draw a new card❓")
 }
 else if (sum === 21) {
     console.log("You have got Blackjack! 🍾🥂")
+    hasBlackJack = true
 }
 else {
     console.log("You are out of the game 😭")
+    isAlive = false
 }
+console.log(hasBlackJack)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
