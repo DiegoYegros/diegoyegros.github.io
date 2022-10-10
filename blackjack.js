61409b88cda780e796f54a1f50cfc0ab783ebeb1
Diego Yegros <diegoyegrosa@gmail.com>
2022-10-09 21:54:38 -0300

    blackjack

--- blackjack.js
+++ blackjack.js
@@ -6,7 +6,7 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-cards-el")
-
+let cardsEl = document.querySelector("#cards-el")
 function startGame() {
     if (sum < 21) {
         message = "Do you want to draw a new card?"
@@ -20,7 +20,11 @@ function startGame() {
         isAlive = false
     }
     messageEl.textContent = message
+    cardsEl.textContent = "Cards: " + firstCard + " and " + secondCard
     sumEl.textContent = "Sum: " + sum
+}
+function newCard() {
+
 }
 
 
